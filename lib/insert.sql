c305ae4e6d57631dd87579de1baa9eb9a88c094c
Blaque <blaque.develop@gmail.com>
2023-08-31 10:39:32 +0300

    Done with assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 12, 'M', 'Black', 'Calm', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'Brown', 'Wild', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 30, 'M', 'Black', 'Intense', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 25, 'F', 'Pink', 'Nice', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 30, 'M', 'Green', 'Grumpy', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 40, 'F', 'Yellow', 'Jolly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 42, 'M', 'Purple', 'Goofy', 0);
+
